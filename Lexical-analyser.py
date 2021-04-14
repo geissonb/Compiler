2742ca0b7ca5a085ec952d5951eae0ef6ef4eb4d
geissonb <geissonbarbacena@gmail.com>
2021-04-14 16:03:31 -0300

    Lexical Analyser

---
+++ Lexical-analyser.py
@@ -0,0 +1,239 @@
+global tabela_simbolos 
+dfa = {
+  0 : {'D': 1, '"': 8, 'L': 10, 'E': 10, 'e': 10, '{': 11, 'eof': 13, '<': 14, '>': 15, '=': 17, '+': 19, '-': 19, '*': 19, '/': 19, '(': 20, ')': 21, ';': 22, ',': 23},
+  1 : {'D': 1, 'E': 4, 'e': 4, '.': 2},
+  2 : {'D': 3},
+  3 : {'D': 3},
+  4 : {'+': 6, '-': 6, 'D': 5},
+  5 : {'D': 5},
+  6 : {'D': 7},
+  7 : {'D': 7},
+  8 : {'"': 9},
+  10 : {'L': 10, 'E': 10, 'e': 10, 'D': 10, '_': 10},
+  11: {'}': 12},
+  14: {'=': 16, '>': 16, '-': 18},
+  15: {'=': 16},
+}
+
+finais = {
+	1 : 'num',
+	3 : 'num',
+	5 : 'num',
+	7 : 'num',
+	9 : 'literal',
+	10: 'id',
+	12: 'comentario',
+	13: 'eof',
+	14: 'opr',
+	15: 'opr',
+	16: 'opr',
+	17: 'opr',
+	18: 'rcb',
+	19: 'opm',
+	20: 'ab_p',
+	21: 'fc_p',
+	22: 'pt_v',
+	23: 'vir'
+}
+
+# descrição de erros de acordo com o estado em que parou
+erros = {
+    0: 'Caractere Inválido!',
+    2: 'Necessita de numero após o ponto.',
+    4: 'Necessita de um sinal(+ ou -) após o exponencial ou dígito.',
+    6: 'Necessita de um número após o sinal(+ ou -)',
+    8: 'Falta fechar aspas!',
+    11: 'Falta fechar chaves!'
+}
+
+# a tabela de símbolos é um dicionário de listas
+# lexema = palavra, token = classe, tipo não está sendo utilizado
+tabela_simbolos = {
+#  Lexema  	     Lexema        Token     Tipo
+  'inicio'   : ['inicio'   , 'inicio'   , ''],
+  'varinicio': ['varinicio', 'varinicio', ''],
+  'varfim'   : ['varfim'   , 'varfim'   , ''],
+  'escreva'  : ['escreva'  , 'escreva'  , ''],
+  'leia'     : ['leia'     , 'leia'     , ''],
+  'se'       : ['se'       , 'se'       , ''],
+  'entao'    : ['entao'    , 'entao'    , ''],
+  'fimse'    : ['fimse'    , 'fimse'    , ''],
+  'fim'      : ['fim'      , 'fim'      , ''],
+  'lit'      : ['lit'      , 'lit'      , ''],
+  'int'      : ['int'      , 'int'      , ''],
+  'real'     : ['real'     , 'real'     , '']
+}
+
+
+# variáveis
+global erro, Lista_tokens, TOKENS
+erro = 1
+TOKENS = {}
+Lista_tokens = []
+
+def OpenFile(name):
+	global source
+	source = open(name)
+
+def CloseFile(name):
+	name.close() 
+
+def verifica_caractere(s):
+
+	car = str(s)
+
+	if car == 'E' or car == 'e': 
+		return s
+
+	elif (car.isalpha() is True): #verifica se é letra 
+		return "L"
+ 
+	elif (car.isdigit() is True): #verifica se é dígito
+		return "D"
+
+	else:
+		return s
+
+def ERROR(text,estado,l,c):
+	a = str(l)
+	b = str(c)
+	print(text + ' - ' + erros[estado]+ " linha " + a + " coluna " + b)
+
+
+def SCANNER():
+	global atual,proximo, buffer, ant, linha, coluna, flag,b,erro
+
+	atual = 0
+	buffer = ''
+	linha = 0
+	coluna = 0
+	flag = False
+	b = ''
+	qtd_erro = erro
+
+	while True:
+		encontrado = False
+
+		ant = source.tell()
+		temp = source.read(1)
+		coluna += 1
+
+		if temp == '':
+			if atual == 0: atual = 13
+
+			elif atual == 8 or atual == 11:
+
+				source.seek(ant,0)
+				a = str(qtd_erro)
+				b = "Erro " + a
+				TOKENS[buffer] = [b, buffer,'']
+				ERROR(b,atual,linha,coluna)
+				erro += 1
+				flag = True
+				return(TOKENS[buffer])
+
+			if atual in finais:
+				TOKENS['eof'] = ['eof','eof','']
+				return(TOKENS['eof'])
+
+
+		else:
+			caractere = verifica_caractere(temp)
+
+			if atual in dfa:
+				for x in dfa[atual]:
+					if	x == caractere:
+						encontrado = True
+						proximo = (dfa[atual])[x]
+						break
+
+			if atual == 8 or atual == 11:
+				if caractere == '\n':
+					linha +=1
+					coluna = 0
+				encontrado = True
+								
+			if encontrado:
+				buffer += temp
+				atual = proximo
+
+			elif caractere in [' ', '\n', '\t']:
+				if atual != 0:
+					if atual in finais:
+						if atual == 10:
+							if buffer in tabela_simbolos:
+								return(tabela_simbolos[buffer])
+							else:
+								tabela_simbolos[buffer] = [finais[atual], buffer, '']
+								return(tabela_simbolos[buffer])
+						elif atual != 10:
+							if buffer in TOKENS:
+								return(TOKENS[buffer])
+							else:
+								TOKENS[buffer] = [finais[atual], buffer, '']
+								return(TOKENS[buffer])
+
+					buffer = ''
+					atual = 0
+
+				if caractere == '\n':
+					linha += 1
+					coluna = 0
+
+			else:
+				source.seek(ant,0)
+				coluna -= 1
+				if atual != 0:
+					if atual in finais:
+						if atual == 10:
+							if buffer in tabela_simbolos:
+								return(tabela_simbolos[buffer])
+							else:	
+								tabela_simbolos[buffer] = [finais[atual], buffer, '']
+								return(tabela_simbolos[buffer])
+						elif atual != 10:
+							if buffer in TOKENS:
+								return(TOKENS[buffer])
+							else:
+								TOKENS[buffer] = [finais[atual], buffer, '']
+								return(TOKENS[buffer])
+					else:
+						a = str(qtd_erro)
+						b = "Erro" + a
+						TOKENS[buffer] = [b,buffer,'']
+						erro += 1
+						flag = True
+						return(TOKENS[buffer])
+
+				else:
+					a = str(qtd_erro)
+					b = "Erro " + a
+					TOKENS[buffer] = [b,caractere,'']
+					erro += 1
+					source.read(1)
+					flag = True
+					return(TOKENS[buffer])
+
+				
+				buffer = ''
+				atual = 0
+
+#----------------------EXECUÇÃO------------------------------------------
+
+OpenFile("fonte.txt")
+
+while True: 
+  s = SCANNER()
+
+  print(s)
+
+  if flag:
+    ERROR(b,atual,linha,coluna)
+
+  if atual == 13:
+    break
+  
+
+  
+
+CloseFile(source)
