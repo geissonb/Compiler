5ff940ccc06f2545a536f7879ba020f08bf9c4af
geissonb <geissonbarbacena@gmail.com>
2023-03-28 16:35:51 -0300

    Analisador Semântico

--- Compilador/semantico.py
+++ Compilador/semantico.py
@@ -42,66 +42,66 @@ gram = {
     38: 'A -> fim',
 }
 
-# buffer para ver o que colocar no programa
-buffer = []
+tipo_var = {
+    'literal': 'char',
+    'inteiro': 'int',
+    'real': 'double'
+}
 
 # pilha_semantica semântica
 pilha_semantica = []
 pilha_semantica_aux = []
+cabecalho = []
+corpo_temporarias = []
+corpo = []
 # nome do arquivo resultado
 resultado = 'PROGRAMA.C'
+
+
+def iniciar():
+
+    cabecalho.append('#include<stdio.h>\n')
+    cabecalho.append('#include<stdlib.h>\n')
+    cabecalho.append('#include<stdbool.h>\n\n')
+    cabecalho.append('typedef char lit[256];\n\n')
+    cabecalho.append('int main(){\n\n')
+
+    corpo_temporarias.append('/*variáveis temporarias*/\n')
+
+    corpo.append('\n\n/*variáveis*/\n')
+
+
+def validar():
+    escrever_arq(cabecalho)
+    escrever_arq(corpo_temporarias)
+    escrever_arq(corpo)
+
 
 # variáveis para gerenciar os temporários
 numTemp = 0
 posTexto = 0
 
+
 # conferir declaração de variável
-
-
 def conferir_declaracao(valores):
     if valores[2] != '':
         return True
     else:
         return False
 
-# função para ir adicionando a um buffer o que tenho que escrever no arquivo
 
+def escrever_arq(buffer):
+    with open(resultado, 'a') as F:
+        F.writelines(buffer)
 
-def addbuff(texto):
-    global buffer
-    buffer.append(texto)
-
-# função para escrever no arquivo ao final
-
-
-def escreverarq():
-    arquivo = open(resultado, 'w')
-    for x in buffer:
-        arquivo.write(x+'\n')
-
-    arquivo.close()
 
 # empilhar na pilha_semantica semantica
-
-
 def empilhar(token, lexema, tipo):
     pilha_semantica.append([token, lexema, tipo])
 
-# inicio do arquivo a escrever
-
-
-def iniciar():
-    global posTexto
-    texto = '#include<stdio.h>\n#include<stdlib.h>\n#include<stdbool.h>\n\ntypedef char lit[256];\n\n' \
-            'int main(){\n\n/*variáveis temporarias*/\n'
-    addbuff(texto)   # adiciono ao buffer o cabeçalho do arquivo
-    posTexto = len(buffer)   # posição no texto para saber onde adicionar os temporários
-    addbuff('\n/*variáveis*/')   # adiciono depois essa string
 
 # função geral do semantico
-
-
-def avalia(prod, qtd_smb):
+def avalia(prod):
     global pilha_semantica
     global numTemp
 
@@ -109,19 +109,7 @@ def avalia(prod, qtd_smb):
 
     # começo a testar as produções
     if prod == gram[5]:   # LV -> varfim
-        addbuff('\n'*3)
-
-    elif prod == gram[9]:  # TIPO -> int
-        aux = pilha_semantica.pop()
-        pilha_semantica.append(aux)
-
-    elif prod == gram[10]:  # TIPO -> real
-        aux = pilha_semantica.pop()
-        pilha_semantica.append(aux)
-
-    elif prod == gram[11]:  # TIPO -> lit
-        aux = pilha_semantica.pop()
-        pilha_semantica.append(aux)
+        corpo.append('\n'*3)
 
     elif prod == gram[6]:   # D -> TIPO L
         i = -1
@@ -133,8 +121,8 @@ def avalia(prod, qtd_smb):
             if Analisador_lexico.tabela_simbolos[pilha_semantica[i][1]][2] != '':
                 break
             Analisador_lexico.tabela_simbolos[pilha_semantica[i][1]][2] = tipo_verifica
-            texto = str(tipo_verifica) + ' ' + pilha_semantica[i][1] + ';'
-            addbuff(texto)
+            texto = tipo_var[tipo_verifica] + ' ' + pilha_semantica[i][1] + ';\n'
+            corpo.append(texto)
             i -= 1
 
     elif prod == gram[7]:  # L -> id, L
@@ -158,25 +146,27 @@ def avalia(prod, qtd_smb):
     elif prod == gram[13]:  # ES -> leia id
         if conferir_declaracao(pilha_semantica[-1]):
             if pilha_semantica[-1][2] == 'literal':
-                addbuff('scanf("%s", &' + pilha_semantica[-1][1]+');')
+                corpo.append('scanf("%s", &' + pilha_semantica[-1][1]+');\n')
             elif pilha_semantica[-1][2] == 'inteiro':
-                addbuff('scanf("%d",&' + pilha_semantica[-1][1]+');')
+                corpo.append('scanf("%d",&' + pilha_semantica[-1][1]+');\n')
             elif pilha_semantica[-1][2] == 'real':
-                addbuff('scanf("%f",&' + pilha_semantica[-1][1]+');')
+                corpo.append('scanf("%f",&' + pilha_semantica[-1][1]+');\n')
         else:
-            print("Variável não declarada linha:"+str(Analisador_lexico.linha), "coluna : " + str(Analisador_lexico.coluna))
+            print("Variável não declarada linha:"+str(Analisador_lexico.linha),
+                  "coluna : " + str(Analisador_lexico.coluna))
             erro = True
 
     elif prod == gram[14]:  # ES -> escreva ARG
         argumento = pilha_semantica[-1][2]
         if argumento == 'literal':
-            addbuff('printf("%s",' + pilha_semantica[-1][1] + ');')
+            corpo.append('printf("%s",' + pilha_semantica[-1][1] + ');\n')
         elif argumento == 'inteiro':
-            addbuff('printf("%d",' + pilha_semantica[-1][1] + ');')
+            corpo.append('printf("%d",' + pilha_semantica[-1][1] + ');\n')
         elif argumento == 'real':
-            addbuff('printf("%f"' + pilha_semantica[-1][1] + ');')
+            corpo.append('printf("%f"' + pilha_semantica[-1][1] + ');\n')
         else:
-            print("Variável não declarada linha:"+str(Analisador_lexico.linha), "coluna : " + str(Analisador_lexico.coluna))
+            print("Variável não declarada linha:"+str(Analisador_lexico.linha),
+                  "coluna : " + str(Analisador_lexico.coluna))
             erro = True
 
     elif prod == gram[15]:  # ARG -> literal
@@ -191,18 +181,26 @@ def avalia(prod, qtd_smb):
 
     elif prod == gram[17]:  # ARG -> id
         if not conferir_declaracao(pilha_semantica[-1]):
-            print("Variável não declarada linha:"+str(Analisador_lexico.linha), "coluna : " + str(Analisador_lexico.coluna))
+            print("Variável não declarada linha:"+str(Analisador_lexico.linha),
+                  "coluna : " + str(Analisador_lexico.coluna))
             erro = True
 
     elif prod == gram[19]:  # CMD -> id rcb LD
         x1 = Analisador_lexico.tabela_simbolos[pilha_semantica[-1][1]][2]
-        x2 = pilha_semantica[-3][2]
+        x2 = pilha_semantica[-2][2]
+        flag = True
+        if x2 == '':
+            flag = False
+            x2 = pilha_semantica[-3][2]
 
         if x1 != x2:
-            print("Tipos incompatíveis na atribuição linha : "+ str(Analisador_lexico.linha))
+            print("Tipos incompatíveis na atribuição linha : " + str(Analisador_lexico.linha))
             erro = True
 
-        text = str(pilha_semantica[-1][1]) + '=' + str(pilha_semantica[-2][1]) + ';'
+        text = str(pilha_semantica[-1][1]) + '=' + str(pilha_semantica[-2][1]) + ';\n'
+
+        if flag:
+            corpo.append(text)
 
     elif prod == gram[20]: 	# LD -> OPRD opm OPRD
         x1 = pilha_semantica[-3][2]
@@ -212,11 +210,15 @@ def avalia(prod, qtd_smb):
             print("Erro semântico linha : " + str(Analisador_lexico.linha))
             erro = True
 
-        text = str(x1) + ' T' + str(numTemp) + ' = ' + str(pilha_semantica[-3][1]) + '' + str(pilha_semantica[-1][1]) + \
-               '' + str (pilha_semantica[-2][1]) + ';'
-        addbuff(text)
-        text2 = str(pilha_semantica[-3][1]) + ' = ' + 'T' + str(numTemp) + ';'
-        addbuff(text2)
+        text0 = tipo_var[x1] + ' T' + str(numTemp) + ';\n'
+        text = 'T' + str(numTemp) + ' = ' + str(pilha_semantica[-3][1])\
+               + '' + str(pilha_semantica[-1][1]) + \
+               '' + str(pilha_semantica[-2][1]) + ';\n'
+
+        corpo_temporarias.append(text0)
+        corpo.append(text)
+        text2 = str(pilha_semantica[-3][1]) + ' = ' + 'T' + str(numTemp) + ';\n'
+        corpo.append(text2)
         numTemp += 1
 
     elif prod == gram[23]:  # OPRD -> num
@@ -232,13 +234,15 @@ def avalia(prod, qtd_smb):
         x2 = pilha_semantica.pop()
         x3 = pilha_semantica.pop()
         exp_r = str(x3[1]) + ' ' + str(x1[1]) + ' ' + str(x2[1])
-        text = 'bool ' + 'T' + str(numTemp) + ' = ' + str(exp_r) + ';'
-        text2 = "if (" + 'T' + str(numTemp) + ') {'
-        addbuff(text)
-        addbuff(text2)
+        text0 = 'bool' + ' T' + str(numTemp) + ';\n'
+        text = 'T' + str(numTemp) + ' = ' + str(exp_r) + ';\n'
+        text2 = "if (" + 'T' + str(numTemp) + ') {\n'
+        corpo_temporarias.append(text0)
+        corpo.append(text)
+        corpo.append(text2)
         numTemp += 1
 
     elif prod == gram[31] or prod == gram[37] or prod == gram[38]:
-        addbuff('}')
+        corpo.append('}\n')
 
     return erro
