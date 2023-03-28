58f22e67e51fd07074df5110725c2e251f7aeec3
geissonb <geissonbarbacena@gmail.com>
2023-03-28 16:36:19 -0300

    Analisador Sintático

--- Compilador/sintatico.py
+++ Compilador/sintatico.py
@@ -93,22 +93,22 @@ def tratamento_erros():
     while token not in cnj:
         token, lexema, tipo = Analisador_lexico.gerencia()
 
-    aux = []
+    lista_aux = []
 
-    for x in range (0,75): # total de linhas
-        k = term.loc[x, token] # procuro qual estado tem um shift
+    for x in range(0, 75):  # total de linhas
+        k = term.loc[x, token]  # procuro qual estado tem um shift
         if k[0] == 'S':
-            aux.append(x)
+            lista_aux.append(x)
 
-    x = max(aux)
+    x = max(lista_aux)
 
     while x not in pilha:
-        aux = aux[:-1]
-        x = max(aux)
+        lista_aux = lista_aux[:-1]
+        x = max(lista_aux)
 
     if x in pilha:
         estado = pilha[-1]
-        while estado != x: # tira da pilha até achar algum estado que ajude o processamento
+        while estado != x:  # tira da pilha até achar algum estado que ajude o processamento
             pilha = pilha[:-1]
             estado = pilha[-1]
     else:
@@ -119,12 +119,15 @@ def tratamento_erros():
 
 # ---------- EXECUÇÃO ----------
 
+
+count = 0
+
 # personalização do início
 inicio(arquivo)
 
 # pego os valores das tabelas shift reduce
-nao_term = pd.read_csv('tabela_sintatica_NAO_terminais.csv', index_col = 0)
-term = pd.read_csv('tabela_sintatica_terminais.csv', index_col = 0)
+nao_term = pd.read_csv('tabela_sintatica_NAO_terminais.csv', index_col=0)
+term = pd.read_csv('tabela_sintatica_terminais.csv', index_col=0)
 
 # abrir o arquivo
 Analisador_lexico.OpenFile(arquivo)
@@ -133,19 +136,18 @@ Analisador_lexico.OpenFile(arquivo)
 token, lexema, tipo = Analisador_lexico.gerencia()
 
 # inicio a pilha
-pilha = []
-pilha.append(0)
+pilha = [0]
 aux = ''
 
 # inicio a variavel
-erro = False
+erro_sintatico = False
+erro_semantico = False
+
 
 semantico.iniciar()
 
 # inicio o processamento
 while True:
-
-    #print(pilha)
 
     # pego o estado no topo da pilha
     s = pilha[-1]
@@ -155,38 +157,38 @@ while True:
 
     # se for do tipo shift (empilhar)
     if acao[0] == 'S':
-        pilha.append(int(acao.lstrip('S'))) # empilho o estado a ir
-        semantico.pilha_semantica_aux.append([token,lexema,tipo])
-        token, lexema, tipo = Analisador_lexico.gerencia() # leio o proximo lexema
-
+        pilha.append(int(acao.lstrip('S')))  # empilho o estado a ir
+        semantico.pilha_semantica_aux.append([token, lexema, tipo])
+        token, lexema, tipo = Analisador_lexico.gerencia()  # leio o proximo lexema
 
     # se for do tipo reduce (reduzir)
     elif acao[0] == 'R':
-        prod = gram[acao.lstrip('R')] # pego a produção da gramática referente
-        lado_esq = prod.split(' -> ')[0] # pego o lado esquerdo dividindo pelo ->
-        qtd_smb = len(prod.split(' -> ')[1].split(' ')) # pego a qtd de simbolos do lado direito do ->
-        pilha = pilha[:-qtd_smb] # retiro da pilha a quantidade de simbolos do lado direito
+        prod = gram[acao.lstrip('R')]  # pego a produção da gramática referente
+        lado_esq = prod.split(' -> ')[0]  # pego o lado esquerdo dividindo pelo ->
+        qtd_smb = len(prod.split(' -> ')[1].split(' '))  # pego a qtd de simbolos do lado direito do ->
+        pilha = pilha[:-qtd_smb]  # retiro da pilha a quantidade de simbolos do lado direito
 
-        s = pilha[-1] # pegando o estado ao topo da pilha
-        pilha.append(int(nao_term.loc[s, lado_esq])) # empilhando a transição GOTO[s, prod. esquerda]
+        s = pilha[-1]  # pegando o estado ao topo da pilha
+        pilha.append(int(nao_term.loc[s, lado_esq]))  # empilhando a transição GOTO[s, prod. esquerda]
         print('Topo da pilha: %#2.d. Produção: %#2.d) %s' % (s, int(acao.lstrip('R')), prod))
 
         for i in range(qtd_smb):
             aux = semantico.pilha_semantica_aux.pop()
-            if aux is not None and aux[0] in ['id','int','real','lit','num','opm','opr','literal']:
+            if aux is not None and aux[0] in ['id', 'int', 'real', 'lit', 'num', 'opm', 'opr', 'literal']:
                 semantico.pilha_semantica.append(aux)
 
         semantico.pilha_semantica_aux.append(None)
 
-        erro = semantico.avalia(prod,qtd_smb)
+        erro_semantico = semantico.avalia(prod)
 
-        if erro:
-            print(erro)
+        if erro_semantico:
+            print(erro_semantico)
+            count += 1
             continue
 
     # se for Accept
     elif acao == 'Acc':
-        if not erro:
+        if not erro_sintatico:
             print("\n--- Código totalmente aceito! ---")
         else:
             print("\n--- Código terminado mas houveram erros. ---")
@@ -194,14 +196,14 @@ while True:
 
     # se não for nada acima, então é um erro
     else:
-        print("--- Erro Sintático %s --- " % (erros[acao]),end='')
-        print("Linha: " + str(Analisador_lexico.linha)," Coluna: " + str(Analisador_lexico.coluna))
-        #erro = tratamento_erros()
+        print("--- Erro Sintático %s --- " % (erros[acao]), end='')
+        print("Linha: " + str(Analisador_lexico.linha), " Coluna: " + str(Analisador_lexico.coluna))
+        # erro_sintatico = tratamento_erros()
+
 
 # fecho o arquivo
 Analisador_lexico.CloseFile()
 
-
-if not erro:
-    semantico.escreverarq()
-
+# escrevo o arquivo C
+if count == 0:
+    semantico.validar()
