f1119d47b6b61d081f99c13a790e2a5b2161b3c7
geissonb <geissonbarbacena@gmail.com>
2023-03-29 10:32:47 -0300

    Código Objeto em C

--- Compilador/PROGRAMA.C
+++ Compilador/PROGRAMA.C
@@ -6,48 +6,48 @@ typedef char lit[256];
 
 int main(){
 
- /*variáveis temporarias*/
- bool T0;
- bool T1;
- int T2;
- int T3;
- int T4;
+    /*variáveis temporarias*/
+    bool T0;
+    bool T1;
+    int T2;
+    int T3;
+    int T4;
 
 
- /*variáveis*/
- char A;
- char J;
- char K;
- int B;
- int D;
- double C;
+    /*variáveis*/
+    char A;
+    char J;
+    char K;
+    int B;
+    int D;
+    double C;
 
 
 
- printf("%s","Digite B");
- scanf("%d",&B);
- printf("%s","Digite A:");
- scanf("%s", &A);
- T0 = B > 2;
- if (T0) {
-   T1 = B <= 4;
-   if (T1) {
-     printf("%s","B esta entre 2 e 4");
-   }
- }
- T2 = B+1;
- B = T2;
- T3 = B+2;
- B = T3;
- T4 = B+3;
- B = T4;
- D=B;
- C=5.0;
- printf("%s","\nB=\n");
- printf("%d",D);
- printf("%s","\n");
- printf("%f"C);
- printf("%s","\n");
- printf("%s",A);
- printf("%s","\n");
+    printf("%s","Digite B");
+    scanf("%d",&B);
+    printf("%s","Digite A:");
+    scanf("%s", &A);
+    T0 = B > 2;
+    if (T0){
+        T1 = B <= 4;
+        if (T1){
+            printf("%s","B esta entre 2 e 4");
+        }
+    }
+    T2 = B+1;
+    B = T2;
+    T3 = B+2;
+    B = T3;
+    T4 = B+3;
+    B = T4;
+    D=B;
+    C=5.0;
+    printf("%s","\nB=\n");
+    printf("%d",D);
+    printf("%s","\n");
+    printf("%f"C);
+    printf("%s","\n");
+    printf("%s",A);
+    printf("%s","\n");
 }
